568b0b88919a7d327f44efcc909b15a367b77b5a
tpreston96 <tpreston96@gmail.com>
2021-02-21 17:53:58 -0600

    helper functions

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => {
+  const onTitleClick = (idx) => {
+    console.log("Title clicked", idx);
+  };
+
+  const renderedItems = items.map((item, idx) => {
     return (
       <React.Fragment key={item.title}>
-        <div className="title active">
+        <div className="title active" onClick={() => onTitleClick(idx)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
