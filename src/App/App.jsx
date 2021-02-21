34739ecabeda034f3aba1601ac6e0ef3a672ce7c
tpreston96 <tpreston96@gmail.com>
2021-02-21 17:16:21 -0600

    rebuild App component

---
+++ src/App/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    state = {  }
+    render() { 
+        return ( <div>Hi there!</div> );
+    }
+}
+ 
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App/App'
 
 ReactDOM.render(
-  <div>Hi there!</div>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
