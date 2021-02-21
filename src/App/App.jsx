5f7710cda4b30c038f44ffa17754cdd12829129d
tpreston96 <tpreston96@gmail.com>
2021-02-21 17:43:49 -0600

    Building the Accordion component

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Accordion from "../components/Accordion/Accordion";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
 
 class App extends Component {
-    state = {  }
-    render() { 
-        return ( <div>Hi there!</div> );
-    }
+  state = {};
+  render() {
+    return (
+      <div>
+        <Accordion items={items} />
+      </div>
+    );
+  }
 }
- 
+
 export default App;
