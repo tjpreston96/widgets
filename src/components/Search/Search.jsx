1f94de9c4acd72e70b0c9dccd77d3c4c05b1dcd2
tpreston96 <tpreston96@gmail.com>
2021-02-21 20:36:22 -0600

    useEffect

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+console.log('I RUN WITH EVERY RENDER')
+  useEffect(() => {
+    console.log("I ONLY RUN ONCE");
+  }, [term]);
   return (
     <div>
       <div className="ui form">
