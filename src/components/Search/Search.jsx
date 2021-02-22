b76c026472bfa6e9ddfbcae425b5807e918e9af0
tpreston96 <tpreston96@gmail.com>
2021-02-21 22:18:26 -0600

    mapping results

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -21,6 +21,17 @@ const Search = () => {
     search();
   }, [term]);
 
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          {result.snippet}
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <div className="ui form">
@@ -34,6 +45,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
