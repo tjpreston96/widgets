8ad9e07a2131e211e596901921f8f135691bf1fa
tpreston96 <tpreston96@gmail.com>
2021-02-21 22:57:09 -0600

    setTimeout

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -18,9 +18,12 @@ const Search = () => {
       });
       setResults(data.query.search);
     };
-    if (term) {
-      search();
-    }
+    
+    const timeoutId = setTimeout(() => {
+      if (term) {
+        search();
+      }
+    }, 500);
   }, [term]);
 
   const renderedResults = results.map((result) => {
