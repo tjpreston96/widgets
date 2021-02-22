8d05e0a8a6476e34c46cf7383c5376cf0b1869e5
tpreston96 <tpreston96@gmail.com>
2021-02-21 19:03:38 -0600

    conditional rendering

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -4,25 +4,27 @@ const Accordion = ({ items }) => {
   const [activeIndex, setActiveindex] = useState(null);
 
   const onTitleClick = (idx) => {
-    setActiveindex(idx)
+    setActiveindex(idx);
   };
 
   const renderedItems = items.map((item, idx) => {
+    const active = idx === activeIndex ? "active" : "";
+
     return (
       <React.Fragment key={item.title}>
-        <div className="title active" onClick={() => onTitleClick(idx)}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(idx)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
-  return <div className="ui styled accordion">{renderedItems}
-  <h1>{activeIndex}</h1>
-  </div>;
+
+  //
+  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
