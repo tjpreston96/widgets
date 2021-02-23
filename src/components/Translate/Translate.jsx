f38d136032f57037ea1514c88a5c0db8e9507d19
tpreston96 <tpreston96@gmail.com>
2021-02-22 19:22:17 -0600

    Convert component

--- src/components/Translate/Translate.jsx
+++ src/components/Translate/Translate.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import Dropdown from "../Dropdown/Dropdown";
+import Convert from "../Convert/Convert";
+
+// todo --> AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
+
 const options = [
   { label: "Afrikaans", value: "af" },
   { label: "Arabic", value: "ar" },
@@ -43,6 +47,9 @@ const Translate = () => {
         onSelectedChange={setLanguage}
         options={options}
       />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert language={language} text={text} />
     </div>
   );
 };
