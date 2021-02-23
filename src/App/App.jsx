ffbdba34fb4342e3a7e0ae41b8a71a500f2e95eb
tpreston96 <tpreston96@gmail.com>
2021-02-23 15:42:27 -0600

    basic navigation

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,24 +1,26 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React, { useState } from "react";
-// import Accordion from "../components/Accordion/Accordion";
-// import Search from "../components/Search/Search";
+import Accordion from "../components/Accordion/Accordion";
+import Search from "../components/Search/Search";
 import Dropdown from "../components/Dropdown/Dropdown";
 import Translate from "../components/Translate/Translate";
+import Route from "../components/Route/Route";
+import Header from "../components/Header/Header";
 
-// const items = [
-//   {
-//     title: "What is React?",
-//     content: "React is a front end javascript framework",
-//   },
-//   {
-//     title: "Why use React?",
-//     content: "React is a favorite JS library among engineers",
-//   },
-//   {
-//     title: "How do you use React?",
-//     content: "You use React by creating components",
-//   },
-// ];
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
 
 const options = [
   { label: "The Color Red", value: "red" },
@@ -27,9 +29,27 @@ const options = [
 ];
 
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      <Translate />
+      <Header/>
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
